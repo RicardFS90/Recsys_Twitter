910b186182b2941f0fe8bbd4a2db5e537dc7a393
Pere <pereblanch14@gmail.com>
2020-04-30 17:53:29 +0200

    adding a random file

---
+++ prova.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import matplotlib
+
+# Data Import
+# -------------------------------------------------------------------------------------------------------------------
+
+# set path
+libname = 'C:/Users/usuario/Documents/Data Science/Postgrau-master/Final Project'
+
+# load data
+train_data = pd.read_csv(os.path.join(libname, 'tocho.csv'))
+
+
